5b3bd09503d726b58fd12fb08dce221e9a3439a7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-09 00:22:03 -0300

    Enviando requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+
+    found_news = search_news(query)
+
+    return [
+        (news["title"], news["url"])
+        for news in found_news
+    ]
 
 
 # Requisito 7
