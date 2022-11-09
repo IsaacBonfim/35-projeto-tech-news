001bc5e93878b41011c084a215f9f9b672a8f894
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-08 23:05:55 -0300

    Enviando requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,26 @@
+import time
+import requests
+from requests import ReadTimeout
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+
+    try:
+        result = requests.get(
+            url,
+            timeout=3,
+            headers={"user-agent": "Fake user-agent"}
+        )
+
+        if result.status_code == 200:
+            return result.text
+        else:
+            return None
+
+    except ReadTimeout:
+        return None
 
 
 # Requisito 2
