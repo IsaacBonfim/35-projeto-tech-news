28f1a9cf6cb2c086b36274db10ecbf3ef581abc0
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-09 00:44:50 -0300

    Enviando requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+
+    sorted_news = sorted(
+        news_list, key=lambda news: news["comments_count"], reverse=True
+    )
+
+    return [
+        (news["title"], news["url"])
+        for news in sorted_news
+        if sorted_news.index(news) < 5
+    ]
 
 
 # Requisito 11
