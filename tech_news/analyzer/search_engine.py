141bb7e5c097a7ed4f748e04a7b9fd7c57898f56
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-09 00:30:32 -0300

    Enviando requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -15,7 +16,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        query_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+
+        found_news = search_news({"timestamp": query_date})
+
+        return [
+            (news["title"], news["url"])
+            for news in found_news
+        ]
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
